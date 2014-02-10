f7862d75fe93e84e20f139e92fb99ca2bbe305c2
Ville Juutilainen <vjuutilainen@gmail.com>
2014-02-10 16:37:09 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+
+window.onload = function(){
+
+	var svg = d3.select("body").append("svg").attr("width",WIDTH).attr("height",HEIGHT);
+
+}
