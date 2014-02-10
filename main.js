24cc9a4e9727bc52094e450f8073f0e37ebf47be
Ville Juutilainen <vjuutilainen@gmail.com>
2014-02-10 18:46:47 -0500

    added circle as the "body" of the globe

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ window.onload = function(){
 	var svg = d3.select("body").append("svg").attr("width",width).attr("height",height);
 	var projection = d3.geo.orthographic().rotate([0,0]).center([0,0]).scale(scale).translate([width/2, height/2]).clipAngle(90);
 	var path = d3.geo.path().projection(projection);
+
+	svg.append("circle").attr("cx",width/2).attr("cy",height/2).attr("r",height/2);
 	
 	d3.json("countries.json",function(data){
 
@@ -27,10 +29,10 @@ window.onload = function(){
 	moveMap = function(){
 
 		setTimeout(function() {
-        
 			currentRotation = projection.rotate()[0];
 			nextRotation = (currentRotation-1)%360;
 			projection.rotate([nextRotation,0]);
+			
 			mapPaths.attr("d",path);
 	        
 	        requestAnimationFrame(moveMap);
