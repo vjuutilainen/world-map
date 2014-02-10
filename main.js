dbab0099e6a545dee2c61077bd5ca81cdd3a7100
Ville Juutilainen <vjuutilainen@gmail.com>
2014-02-10 18:38:47 -0500

    added clipAngle

--- main.js
+++ main.js
@@ -7,11 +7,10 @@ var mapPaths;
 window.onload = function(){
 
 	var svg = d3.select("body").append("svg").attr("width",width).attr("height",height);
-	var projection = d3.geo.orthographic().rotate([0,0]).scale(scale).translate([width/2, height/2]);
+	var projection = d3.geo.orthographic().rotate([0,0]).center([0,0]).scale(scale).translate([width/2, height/2]).clipAngle(90);
 	var path = d3.geo.path().projection(projection);
 	
 	d3.json("countries.json",function(data){
-		console.log(data);
 
 		mapPaths = svg.selectAll("path")
 			.data(data.features)
@@ -19,9 +18,8 @@ window.onload = function(){
 			.append("path")
 			.attr("d",path);
 
-
-
 		moveMap(mapPaths);
+		
 
 	})
 
@@ -31,7 +29,7 @@ window.onload = function(){
 		setTimeout(function() {
         
 			currentRotation = projection.rotate()[0];
-			nextRotation = currentRotation+1;
+			nextRotation = (currentRotation-1)%360;
 			projection.rotate([nextRotation,0]);
 			mapPaths.attr("d",path);
 	        
